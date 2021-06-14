999dd7a97b62867e71b6e6207dc004813088acae
shooteram <me@shooteram.fr>
2021-06-14 17:21:35 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import { app, BrowserWindow } from 'electron';
 
-const createWindow = () => {
-    (new BrowserWindow({ width: 800, height: 600 }))
-        .loadFile('index.html');
+if (!process.env.PEPFLASHPLAYER_PATH) {
+    console.error('no path set to pepflashplayer');
+    process.exit(1);
 }
 
-app.whenReady().then(() => { createWindow() });
+app.commandLine.appendSwitch('ppapi-flash-path', process.env.PEPFLASHPLAYER_PATH);
+app.commandLine.appendSwitch('ppapi-flash-version', process.env.PEPFLASHPLAYER_VERSION || '32.0.0.465');
+
+app.whenReady().then(() => {
+    (new BrowserWindow({ webPreferences: { plugins: true, webviewTag: true } })).loadFile('./index.html');
+});
